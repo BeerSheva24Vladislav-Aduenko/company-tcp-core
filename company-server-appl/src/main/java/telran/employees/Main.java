5ac28b87ba55c7f4bf22f46d9ecab8837ace20a8
VladislavAduenko <adyenko.job@gmail.com>
2024-12-26 01:53:38 +0200

    fix code

--- company-server-appl/src/main/java/telran/employees/Main.java
+++ company-server-appl/src/main/java/telran/employees/Main.java
@@ -1,22 +1,24 @@
 package telran.employees;
 
-import telran.io.*;
-import telran.net.*;
+import telran.io.Persistable;
+import telran.io.PersistableSaverThread;
+import telran.net.TcpServer;
 
 public class Main {
     private static final String FILE_NAME = "employees.data";
         private static final int PORT = 4000;
-        
-            public static void main(String[] args) {
-                Company company = new CompanyImpl();
-                if (company instanceof Persistable persistable) {
-                    persistable.restoreFromFile(FILE_NAME);
+                private static final long TIME_INTERVAL = 60000;
+                
+                    public static void main(String[] args) {
+                        Company company = new CompanyImpl();
+                        if (company instanceof Persistable persistable) {
+                            persistable.restoreFromFile(FILE_NAME);
+                            PersistableSaverThread saverThread = new PersistableSaverThread(persistable, FILE_NAME, TIME_INTERVAL);
+                            saverThread.start();
+                            Runtime.getRuntime().addShutdownHook(new Thread(() -> persistable.saveToFile(FILE_NAME)));
             }
-            Protocol protocol = new CompanyProtocol(company);
-            TCPServer server = new TCPServer(protocol, PORT);
-            Thread threadServer = new Thread(server);
-            threadServer.start();
-            SaverToFile saver = new SaverToFile(company);
-            saver.start();
+           
+            TcpServer tcpServer = new TcpServer(new CompanyProtocol(company), PORT);
+            tcpServer.run();
     }
 }
